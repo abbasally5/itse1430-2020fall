1c5e29f88253366d3c5663c1b596645f866f4d7e
Michael Taylor <CoolDadTx@outlook.com>
2020-09-21 21:50:45 -0500

    Added Movie class and some fields. Starting adding Movie to UI. Added dependency on MovieLibrary from UI. Next time will fix accessibility error.

--- classwork/MovieLibrary/MovieLibrary.WinformsHost/MainForm.cs
+++ classwork/MovieLibrary/MovieLibrary.WinformsHost/MainForm.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;    //DO NOT DELETE
 using System.Windows.Forms;
+
+//Hierarchical namesapces
+//namespace MovieLibrary
+//{
+//    namespace WinformsHost
+//    {
+//    }
+//}
+//namespace Company.Product.<area>
+//namespace Microsoft.Office.Word
+    //namespace Microsoft.Office.Excel
 
 namespace MovieLibrary.WinformsHost
 {
@@ -15,7 +19,15 @@ namespace MovieLibrary.WinformsHost
         public MainForm()
         {
             InitializeComponent();
-        }
 
+            Movie movie;
+        }
     }
 }
+
+//namespace OtherNamespace
+//{
+//    public class MainForm
+//    {
+//    }
+//}
--- classwork/MovieLibrary/MovieLibrary.WinformsHost/Program.cs
+++ classwork/MovieLibrary/MovieLibrary.WinformsHost/Program.cs
@@ -1,8 +1,7 @@
+//using directive - imports all identifiers from a namespace
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+//using System.Windows.Forms;
 
 namespace MovieLibrary.WinformsHost
 {
@@ -14,10 +13,13 @@ namespace MovieLibrary.WinformsHost
         [STAThread]
         static void Main()
         {
+            //System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            //Application.Run(new MovieLibrary.WinformsHost.MainForm());
         }
     }
 }
