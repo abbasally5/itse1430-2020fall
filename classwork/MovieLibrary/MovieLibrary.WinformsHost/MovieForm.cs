b2bee67421dbd7e7e0a4fa030ad2928b4e275c39
Michael Taylor <CoolDadTx@outlook.com>
2020-09-23 21:49:59 -0500

    Added child form to collection movie data. Added start of main menu. Manually hooked up movie add for demo.

---
+++ classwork/MovieLibrary/MovieLibrary.WinformsHost/MovieForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MovieLibrary.WinformsHost
+{
+    public partial class MovieForm : Form
+    {
+        public MovieForm ()
+        {
+            InitializeComponent();
+        }
+
+        private void _txtDescription_TextChanged ( object sender, EventArgs e )
+        {
+
+        }
+    }
+}
